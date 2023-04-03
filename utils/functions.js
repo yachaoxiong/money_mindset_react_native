da5c93984bdb3bdc03253f7eb4a6b2dc0bf5758d
xieyz10 <54917731+xieyz10@users.noreply.github.com>
2023-04-03 11:12:01 -0400

    Analysis screen Leaderboard section

--- utils/functions.js
+++ utils/functions.js
@@ -26,8 +26,29 @@ export const removeToken = async () => {
 }
 
 export const getTodayDate = () => {
- const currentDate = new Date().toISOString().slice(0, 10);
- return " "+currentDate;
+  const currentDate = new Date().toISOString().slice(0, 10);
+  return " " + currentDate;
+}
+
+export const groupbillTypeAmountByPeriod = (bills,billType) => {
+  let map = new Map();
+  let totolIncome = 0;
+  bills.forEach((bill) => {
+    bill.billItems.forEach((billItem) => {
+      if(billItem.billType === billType && Number(billItem.amount)!==0){
+        if(!map.has(billItem.iconName)){
+          map.set(billItem.iconName, Number(billItem.amount));
+        }else{
+          map.set(billItem.iconName, map.get(billItem.iconName) + Number(billItem.amount));
+        }
+        totolIncome+=Number(billItem.amount);
+      }
+    })
+  });
+  const arr = Array.from(map, function (entry) {
+    return { key: entry[0], value: entry[1] };
+  });
+  return [arr,totolIncome];
 }
 
 
