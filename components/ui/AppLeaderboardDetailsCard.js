276b06b9b2841921a4bd69a56b2c540338152247
xieyz10 <54917731+xieyz10@users.noreply.github.com>
2023-04-19 14:35:47 -0400

    delete assets and update assets functions

--- components/ui/AppLeaderboardDetailsCard.js
+++ components/ui/AppLeaderboardDetailsCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import AppNewFlowTypeItem from './AppNewFlowTypeItem';
+import AppStaticFlowTypeItem from './AppStaticFlowTypeItem';
 import { Dimensions } from 'react-native';
 import styles from './styles/appLeaderboardDetailsCardStyle';
 
@@ -12,7 +12,7 @@ export default AppLeaderboardDetailsCard = (props) => {
     return (
         <View style={styles.container}>
             <View style={styles.icon}>
-                <AppNewFlowTypeItem icon = {icon}/>
+                <AppStaticFlowTypeItem icon = {icon}/>
             </View>
             <View style={styles.progressContainer}>
                 <View style={[styles.headerInfo,{width:screenWidth*0.75}]}>
--- screens/AssetsScreen.js
+++ screens/AssetsScreen.js
@@ -1,28 +1,37 @@
-import React, {useState,useContext} from 'react';
+import React, {useState,useContext,useEffect} from 'react';
 import { StoreContext } from '../store/store';
-import { View, SafeAreaView, ScrollView, Text, Image, TouchableOpacity } from 'react-native';
+import { View, SafeAreaView, ScrollView, Text, TouchableOpacity, TouchableHighlight } from 'react-native';
 import { Dimensions } from 'react-native';
 import AppAssetsHeader from '../components/ui/AppAssetsHeader';
 import AppAddNewAssetsModel from '../components/ui/AppAddNewAssetsModel';
 import AppAssetsDetailsCard from '../components/ui/AppAssetsDetailsCard';
 import AppAddNewAssetsDetailsModel from '../components/ui/AppAddNewAssetsDetailsModel';
-import { addNewAssets } from '../services/assetsService';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faXmark, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { addNewAssets, updateAsset } from '../services/assetsService';
+import AppInput from '../components/ui/AppInput';
+import AppButton from '../components/ui/AppButton';
 import useAssets from '../hooks/useAssets';
+import Modal from "react-native-modal";
 import styles from './styles/useAssetsStyle';
 
 export default AssetsScreen = () => {
     const [openAddNewModel, setOpenAddNewModel] = useState(false);
     const { setIsRefreshing } = useContext(StoreContext);
+    const [isVisible, setIsVisible] = useState(false);
     const [tab,setTab] = useState('');
+    const [selectedAsset, setSelectedAsset] = useState('');
     const [bank, setBank] = useState('');
     const [last4Digits,setLast4Digits] = useState('');
     const [note, setNote] = useState('');
     const [amount, setAmount] = useState('');
+    const [itemtab, setItemtab] = useState('');
+    const [itemAmount,setItemAmount] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const screenHeight = Dimensions.get('window').height;
     const assets = useAssets();
     
-    console.log("assets=",assets);
+    // console.log("assets=",assets);
 
     const submit = () => {
         if(tab === 'Cash' || tab === 'Debt'){
@@ -56,6 +65,18 @@ export default AssetsScreen = () => {
         })
     }
 
+    const handleUpdateAsset = async () => {
+        console.log("selectedAsset is",selectedAsset);
+        selectedAsset.amount = +itemAmount;
+        await updateAsset(selectedAsset);
+        setIsVisible(false);
+    }
+
+    useEffect(() => {
+        selectedAsset && setItemtab(selectedAsset.tab)
+        selectedAsset && setItemAmount(selectedAsset.amount.toString());
+    }, [selectedAsset])
+
     return (
         <View style={styles.container}>
             <SafeAreaView>
@@ -65,7 +86,7 @@ export default AssetsScreen = () => {
                 <View style={openAddNewModel || tab !== '' ?styles.content_alt:styles.content}>
                     <ScrollView style={{marginBottom:280}}>
                         {assets.map((asset,index)=>{
-                            return <AppAssetsDetailsCard asset={asset} key={index}/>
+                            return <AppAssetsDetailsCard asset={asset} key={index} setIsVisible={setIsVisible} setSelectedAsset = {setSelectedAsset}/>
                         })}
                     </ScrollView>
                 </View>
@@ -89,6 +110,16 @@ export default AssetsScreen = () => {
                     errorMessage={errorMessage}
                     submit={submit}/>:null}
             </SafeAreaView>
+            <Modal isVisible={isVisible}>
+                <View style={styles.modalContainer}>
+                    <TouchableHighlight onPress={() => setIsVisible(false)} style={styles.closeButton}>
+                        <FontAwesomeIcon icon={faXmark} size={20} color="white" />
+                    </TouchableHighlight>
+                    <AppInput value={itemtab}  onChangeText={text=>setItemtab(text)} editable={false}/>
+                    <AppInput value={itemAmount} onChangeText={text=>setItemAmount(text)} />
+                    <AppButton title="Save"  onPress={handleUpdateAsset} />
+                </View>
+            </Modal>
         </View>
     )
 }
